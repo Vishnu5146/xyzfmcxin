5f7867ddb73f7974918c5cdbb62ba0c48232b57c
NumenticaTechnologies <vishnu.numentica@gmail.com>
2020-01-25 17:43:08 +0530

    Client Build

---
+++ ClientSide/build/precache-manifest.d93822fa23f1f92585645ba53fc4d0b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a776d66b138085fae9ae9d47ef297b71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "798acf2ca3addb9eaf9f",
+    "url": "/static/css/main.9042a276.chunk.css"
+  },
+  {
+    "revision": "6597e25915feeb5cf85a",
+    "url": "/static/js/2.f524cc10.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.f524cc10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "798acf2ca3addb9eaf9f",
+    "url": "/static/js/main.28ba85f5.chunk.js"
+  },
+  {
+    "revision": "28e19365185e5fa92c2b",
+    "url": "/static/js/runtime-main.0d93cba1.js"
+  },
+  {
+    "revision": "df11b0f9a59a7b6b1ed9d0f96805f71f",
+    "url": "/static/media/Background.df11b0f9.jpeg"
+  },
+  {
+    "revision": "0fe5c480795fc5bce75e6bb8c6050003",
+    "url": "/static/media/archery.0fe5c480.png"
+  }
+]);
